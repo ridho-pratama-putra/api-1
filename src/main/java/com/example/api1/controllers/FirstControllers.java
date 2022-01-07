4c2db7abd7406eb30caa3a9f470a2505b160b719
ridho <ridho.putra@btpn.com>
2022-01-08 03:49:35 +0700

    init

---
+++ src/main/java/com/example/api1/controllers/FirstControllers.java
@@ -0,0 +1,15 @@
+package com.example.api1.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstControllers {
+
+    @GetMapping(path = "/first")
+    public ResponseEntity first() {
+        return new ResponseEntity("hello  first", HttpStatus.OK);
+    }
+}
