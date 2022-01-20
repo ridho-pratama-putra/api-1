c97dda8182f50d002d988ed841007060065bdac5
ridho <ridho.putra@btpn.com>
2022-01-20 20:33:37 +0700

    add example controller with role required

--- src/main/java/com/example/api1/controllers/FirstControllers.java
+++ src/main/java/com/example/api1/controllers/FirstControllers.java
@@ -5,11 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.annotation.security.RolesAllowed;
+
 @Controller
 public class FirstControllers {
 
     @GetMapping(path = "/first")
     public ResponseEntity first() {
         return new ResponseEntity("hello  first", HttpStatus.OK);
+    }
+
+    @RolesAllowed("user")
+    @GetMapping(path = "/sec")
+    public ResponseEntity second() {
+        return new ResponseEntity("hello  second", HttpStatus.OK);
     }
 }
