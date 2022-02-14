282b74171e9d41ac0d1e4015dd905174de7dc025
ridho <ridho.putra@btpn.com>
2022-02-14 21:45:30 +0700

    add swagger - http://localhost:8082/swagger-ui.html - http://localhost:8082/v2/api-docs

---
+++ src/main/java/com/example/api1/config/SpringFoxConfig.java
@@ -0,0 +1,9 @@
+package com.example.api1.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+}
